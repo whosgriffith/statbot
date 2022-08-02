fd5cac8a5919543d7a7cb3c8764324182ecd8123
Nicolas <nicolasre011@gmail.com>
2022-08-02 11:08:18 -0300

    Renamed models.py added afk_channel attribute

--- statbot/classes.py
+++ statbot/classes.py
@@ -1,6 +1,6 @@
 class VoiceSession:
     def __init__(self, session_id=None, user_id=None, guild_id=None, channel_id=None, joined_at=None, left_at=None,
-                 total_time=None):
+                 total_time=None, afk_channel=None):
         self.session_id = session_id
         self.user_id = user_id
         self.guild_id = guild_id
@@ -8,3 +8,4 @@ class VoiceSession:
         self.joined_at = joined_at
         self.left_at = left_at
         self.total_time = total_time
+        self.afk_channel = afk_channel
